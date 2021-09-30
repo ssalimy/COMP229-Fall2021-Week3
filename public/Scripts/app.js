d7b9c62a14296ff182318e16087b6767593741a7
ssalimy <ssalimy@my.centennialcollege.ca>
2021-09-30 16:33:03 -0400

    "third commit"

---
+++ public/Scripts/app.js
@@ -0,0 +1,14 @@
+// IIFE Immediately Invoked Function expression 
+(function(){
+
+    function Start()
+    {
+        console.log("App started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
+
+
+
